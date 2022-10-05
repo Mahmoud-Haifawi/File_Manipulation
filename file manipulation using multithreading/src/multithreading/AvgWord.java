acc822f7d8ff992195a89c2532126c3ae4e635b5
Mahmoud Haifawi <75939511+Mahmoud-Haifawi@users.noreply.github.com>
2022-10-05 13:19:59 +0300

    Add files via upload

---
+++ file manipulation using multithreading/src/multithreading/AvgWord.java
@@ -0,0 +1,45 @@
+
+package multithreading;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+
+public class AvgWord implements Runnable {
+  
+static long start = System.currentTimeMillis();
+
+    static ArrayList<String> Words = new ArrayList<>();
+     public AvgWord(ArrayList<String> W ){
+       AvgWord.Words=W;
+    }
+      @Override
+   public void run(){
+    avg( Words);
+   }
+    public static void avg(ArrayList<String> words) {
+      System.out.println("start AvgWord time : " + LocalDateTime.now() );
+        double sum = 0 ;
+        for (String word : Words) {
+            sum+= word.length();  
+            }
+         System.out.println("The AVG word length is : " + sum/Words.size());
+                   long end = System.currentTimeMillis();
+                   
+   //     DateTimeFormatter formatteri = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+NumberFormat formatter = new DecimalFormat("#0.00000");
+// System.out.println("start AvgWord time : " + LocalDateTime.now().format(formatteri) );
+
+         System.out.println("end AvgWord time : " + LocalDateTime.now() );
+System.out.println("Execution  for AvgWord thread time is " + formatter.format((end - start) / 1000d) + " seconds");
+
+        }
+   
+      
+
+
+}
+    
